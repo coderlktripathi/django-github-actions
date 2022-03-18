82e9ccfd5c2b53569255bd957fe183f9526baf25
tripathi221 <laxmikant.tripathi6@gmail.com>
2022-03-18 16:44:19 +0530

    Make post slug unique and add test cases.

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.test import TestCase
 from blog.models import Post
 
@@ -17,3 +18,14 @@ class ModelTesting(TestCase):
         post = self.post
         self.assertTrue(isinstance(post, Post))
         self.assertEqual(str(post), 'django post')
+
+    def test_filter_post_by_author(self):
+        author = self.post.author
+        posts = Post.objects.filter(author=author)
+        self.assertEqual(posts.count(), 1)
+
+    def test_duplicate_slug_not_allowed_in_post(self):
+        try:
+            _ = Post.objects.create(title='test', author='me', slug=self.post.slug)
+        except IntegrityError as ex:
+            self.assertTrue('UNIQUE constraint failed: blog_post.slug' in ex.args)
