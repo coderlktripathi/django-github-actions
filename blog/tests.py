80c40acb8a0c0dbc3103f75443a0f459a20ed833
tripathi221 <laxmikant.tripathi6@gmail.com>
2022-03-18 15:47:18 +0530

    add post model and tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from blog.models import Post
 
 # Create your tests here.
+class ModelTesting(TestCase):
+
+    def setUp(self) -> None:
+        self.post = Post.objects.create(
+            title='django post',
+            author='Adam',
+            slug='adam-django'
+        )
+        return super().setUp()
+
+    def test_post_model(self):
+        post = self.post
+        self.assertTrue(isinstance(post, Post))
+        self.assertEqual(str(post), 'django post')
